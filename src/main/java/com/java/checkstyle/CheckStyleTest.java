f33e94e9d411504417955c4c9a72c2b4c00c021f
Narendra <nknirala.0071@gmail.com>
2018-07-07 23:22:46 +0530

    checkstyle

---
+++ src/main/java/com/java/checkstyle/CheckStyleTest.java
@@ -0,0 +1,24 @@
+package com.java.checkstyle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Java doc.
+ */
+public class CheckStyleTest {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+            emp.setEmpName("Narendra");
+        System.out.println(emp.getEmpName());
+
+        List<String> empList = new ArrayList<String>();
+        //Set
+    }
+
+}
